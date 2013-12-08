05afd9b65d9ae89be1be14a337e5e176609ca46d
pocketberserker <pocketberserker@gmail.com>
2013-12-09 05:38:56 +0900

    chapter2 1 2

--- ContMonadTutorial/Program.fs
+++ ContMonadTutorial/Program.fs
@@ -1,17 +1,13 @@
 ﻿module ContMonadTutorial
 
-let dup x = x * x
+let printResult: int -> unit = printfn "result = %d"
 
-type PrintResultHandler () =
-  member this.PrintResult(result: int) = printfn "result = %d" result
-
-let dupCps (x: int, resultHandler: PrintResultHandler) =
+let dupCps: int -> unit = fun x ->
   let result = x * x
-  resultHandler.PrintResult(result)
+  printResult result
 
 [<EntryPoint>]
 let main _ =
   let x = 10
-  let result = dup x
-  dupCps(x, PrintResultHandler())
+  dupCps x
   0
