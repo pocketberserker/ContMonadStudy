fefc538537fe993618dbdb99c03934eb3292e21c
pocketberserker <pocketberserker@gmail.com>
2013-12-09 06:22:36 +0900

    chapter3 1 2

--- ContMonadTutorial/Program.fs
+++ ContMonadTutorial/Program.fs
@@ -1,17 +1,32 @@
 ﻿module ContMonadTutorial
 
+
+type Cps<'T, 'R> =
+  | Cps of run: (('T -> 'R) -> 'R)
+
+module Cps =
+
+  let private makePassthrough value =
+    fun k -> k value
+
+  let unit' value =
+    let passthrough = makePassthrough value
+    Cps passthrough
+
+  let run k (Cps x) = x k
+
 let printResult: int -> unit = printfn "result = %d"
 
-let makeCps (run: (int -> unit) -> unit) = run
-
-let makeDupCps (x: int) : (int -> unit) -> unit =
-  let dup = fun resultHandler ->
-    let result = x * x
-    resultHandler result
-  makeCps dup
+let makeDupCps (x: int) : Cps<int, unit> =
+  let dup =
+    fun resultHandler ->
+      let result = x * x
+      resultHandler result
+  Cps dup
 
 [<EntryPoint>]
 let main _ =
   let x = 10
-  makeDupCps x printResult
+  makeDupCps x
+  |> Cps.run printResult
   0
