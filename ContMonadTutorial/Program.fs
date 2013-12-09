29374b3a5aca60564548bd45f5244d020525780a
pocketberserker <pocketberserker@gmail.com>
2013-12-09 09:20:03 +0900

    chapter 7 1

--- ContMonadTutorial/Program.fs
+++ ContMonadTutorial/Program.fs
@@ -17,34 +17,9 @@ module Cps =
   let bind f cps =
     Cps (fun k -> cps |> run (fun value -> f value |> run k))
 
-type Arg = {
-  value : int
-  acc : int
-  isAtEnd : bool
-}
-
-[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
-module Arg =
-  let of' v a = { value = v; acc = a; isAtEnd = false }
-  let forResult acc = { value = 0; acc = acc; isAtEnd = true }
-
-let rec fact = function
-| { value = v; acc = a; isAtEnd = isAtEnd } ->
-  if v = 0 then Cps.unit' (Arg.forResult a)
-  else (v - 1, a * v) ||> Arg.of' |> Cps.unit'
-
-let testFact () =
-  let init = (10, 1) ||> Arg.of' |> Cps.unit'
-  let rec loop iter =
-    if (iter |> Cps.run id).isAtEnd then iter
-    else loop (iter |> Cps.run id |> Cps.unit' |> Cps.bind fact)
-  let result =
-    init
-    |> loop
-    |> Cps.run id
-  assert (result.acc = 3628800)
+  let callCC f =
+    Cps (fun k -> f (fun value -> Cps (fun _ -> k value)) |> run k)
 
 [<EntryPoint>]
 let main _ =
-  testFact ()
   0
